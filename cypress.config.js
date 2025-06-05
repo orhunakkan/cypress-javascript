ffe8d6e72d3239781637107bc4279ae578296009
orhunakkan <orhunakkan@outlook.com>
2025-06-05 11:43:48 -0500

    feat: register grep plugin in Cypress configuration

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) { },
+    setupNodeEvents(on, config) {
+      // Register the grep plugin
+      require('@cypress/grep/src/plugin')(config);
+      return config;
+    },
     specPattern: 'cypress/tests/**/*.cy.js',
   },
 });
