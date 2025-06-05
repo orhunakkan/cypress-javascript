bdc95ecb53f4566e762befaf85ffc7d3566480ec
orhunakkan <orhunakkan@outlook.com>
2025-06-05 10:42:55 -0500

    feat: add initial README and Heroku app smoke test

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-    },
+    setupNodeEvents(on, config) { },
+    specPattern: 'cypress/tests/**/*.cy.js',
   },
 });
---
+++ cypress/tests/e2e/heroku-app-smoke.cy.js
@@ -0,0 +1,12 @@
+describe("Heroku App - Smoke Suite @smoke", () => {
+
+    beforeEach(async () => {
+        cy.visit("https://the-internet.herokuapp.com/");
+    });
+
+    it("should load the homepage and validate title and basic elements", async () => {
+        cy.title().should("eq", "The Internet");
+        cy.get("h1").should("contain", "Welcome to the-internet");
+        cy.get("h2").should("contain", "Available Examples");
+    });
+});
