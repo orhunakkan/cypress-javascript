f04fda9b3798f5fb387a442bc736f387d1b75dd1
orhunakkan <orhunakkan@outlook.com>
2025-06-05 11:37:08 -0500

    feat: add additional smoke tests for homepage validation

--- cypress/tests/e2e/heroku-app-smoke.cy.js
+++ cypress/tests/e2e/heroku-app-smoke.cy.js
@@ -10,3 +10,35 @@ describe("Heroku App - Smoke Suite @smoke", () => {
         cy.get("h2").should("contain", "Available Examples");
     });
 });
+
+describe("Heroku App - Smoke Suite", () => {
+
+    beforeEach(async () => {
+        cy.visit("https://the-internet.herokuapp.com/");
+    });
+
+    it("should load the homepage and validate title and basic elements", async () => {
+        cy.title().should("eq", "The Internet");
+        cy.get("h1").should("contain", "Welcome to the-internet");
+        cy.get("h2").should("contain", "Available Examples");
+    });
+});
+
+describe("Heroku App - Smoke Suite", () => {
+
+    beforeEach(async () => {
+        cy.visit("https://the-internet.herokuapp.com/");
+    });
+
+    it("should load the homepage and validate title and basic elements @smoke", async () => {
+        cy.title().should("eq", "The Internet");
+        cy.get("h1").should("contain", "Welcome to the-internet");
+        cy.get("h2").should("contain", "Available Examples");
+    });
+
+    it("should load the homepage and validate title and basic elements", async () => {
+        cy.title().should("eq", "The Internet");
+        cy.get("h1").should("contain", "Welcome to the-internet");
+        cy.get("h2").should("contain", "Available Examples");
+    });
+});
