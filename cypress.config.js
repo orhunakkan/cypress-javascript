0b4e87bdf9b667c61344e3e3b33353305eb7c58f
orhunakkan <orhunakkan@outlook.com>
2025-06-10 18:54:05 -0500

    refactor: enhance Cypress configuration with additional settings

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,20 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) { },
+    setupNodeEvents(on, config) {
+      return config
+    },
     specPattern: 'cypress/tests/**/*.cy.js',
     baseUrl: 'https://the-internet.herokuapp.com/',
     defaultBrowser: 'edge',
+    defaultCommandTimeout: 30000,
+    experimentalRunAllSpecs: true,
+    retries: process.env.CI ? 1 : 0,
+    screenshotOnRunFailure: false,
+    video: false,
+    numTestsKeptInMemory: process.env.CI ? 4 : 50,
   },
+  env: {
+    ignoreHttpErrors: true,
+  }
 })
