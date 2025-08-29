b1af6678d614c599b28bdf40e8094d60446b145f
Orhun Akkan <orhunakkan@outlook.com>
2025-08-29 13:01:50 -0500

    feat: add dotenv dependency and e2e test for homepage URL validation

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
+    specPattern: 'cypress/tests/e2e/**/*.cy.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       return config;
     },
