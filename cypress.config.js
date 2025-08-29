c8cdfea85125c2d4d1787f111fcc6231ad81bc24
orhunakkan <orhunakkan@outlook.com>
2025-08-29 00:25:04 -0500

    chore: starting over

--- cypress.config.js
+++ cypress.config.js
@@ -5,23 +5,5 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       return config;
     },
-    specPattern: 'cypress/tests/**/*.cy.js',
-    baseUrl: process.env.ENV,
-    defaultBrowser: 'edge',
-    defaultCommandTimeout: 30000,
-    experimentalRunAllSpecs: true,
-    retries: process.env.CI ? 1 : 0,
-    screenshotOnRunFailure: true,
-    video: true,
-    reporter: 'mochawesome',
-    reporterOptions: {
-      reportDir: 'cypress/cypress-reports',
-      overwrite: true,
-      html: true,
-      json: true,
-    },
-  },
-  env: {
-    ignoreHttpErrors: true,
   },
 });
